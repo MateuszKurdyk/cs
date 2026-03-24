458a98a10d3770ab703754056219f8b1e049ebdc
Mateusz <mateusz.kurdyk@student.wsb.edu.pl>
2026-03-24 17:05:27 +0100

    dodanie wprowadzania wartosci do konwersji, obsluga złego formatu wyświetlanie wyniku konwersji

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -13,24 +13,41 @@
 
                 if(opcja == "F" || opcja=="f" || opcja=="C" || opcja == "c")
                 {
-                    Console.WriteLine(opcja);
+                    Console.WriteLine("Wybrano konwersję z °"+opcja);
+                    // zastosowanie replace gdy uzytkownik wprowadzi , zamiast .
+                    Console.Write("Podaj wartość: ");
+                    string input = Console.ReadLine();
+                    input = input.Replace(",",".");
+                    double temp = double.Parse(input);
+                    // Console.WriteLine(temp);
+                    double wynik = 0;
+                    // nadpisanie inputa do wyswietlenia wyniku wraz ze skalą 
                     if(opcja == "F" || opcja == "f")
                     {
-                        Console.WriteLine("F na C");
+                        wynik = (temp-32)/1.8;                        
+                        Console.WriteLine("°F na °C");
+                        input=wynik+"°C";
                     }
                     else if(opcja=="C" || opcja == "c")
                     {
-                        Console.WriteLine("C na F");
+                        wynik = temp * 1.8 + 32;
+                        Console.WriteLine("°C na °F");
+                        input=wynik+"°F";
                     }
+                    Console.WriteLine($"Wynik konwersji to {input} ");
                 }
                 else
                 {
-                    throw new InvalidOperationException("nieznana skala");
+                    throw new InvalidOperationException("nieznana skala wybierz F albo C");
                 }           
             }
             catch(InvalidOperationException ex)
             {
                 Console.WriteLine("Błąd: "+ex.Message);   
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Błąd: wprowadzono zły format, wprowadź liczbę");   
             }
             catch
             {
