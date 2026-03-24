01616468ef9074d5dc48ee99ee160281f2cd6d66
Mateusz <mateusz.kurdyk@student.wsb.edu.pl>
2026-03-24 15:17:29 +0100

    walidacja danych wejściowych

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -3,13 +3,42 @@
 
     class ProstyKalkulator
     {
-        static void Main()
+        static void Main(string[] args)
         {
-        
-            // inputy dla liczb i znakow
+            try
+            {
+            // zastosowanie int nie trzeba zmienoprzecinkowych (prosty kalkulator)
+            // ewentualnie long ale int powinien w zupełności wystarczyć
+                Console.Write("Podaj liczbę nr.1: ");           
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Podaj operator (+,-,*,/): ");           
+                string op = Console.ReadLine();
+                Console.Write("Podaj liczbę nr.2: ");           
+                int b = int.Parse(Console.ReadLine());
+                // Walidacja operatora
+                if(op=="+" || op=="-" || op=="*" || op == "/")
+                {
+                    Console.WriteLine(op);
+                }
+                else
+                {
+                    // Dodanie błędu do catch
+                    throw new InvalidOperationException("nieznany operator");
+                }
+            }
+            catch(FormatException)
+            {
+                // błąd przy wprowadzaniu liczby
+                Console.WriteLine("błędny format");
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine("błąd: "+ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("błąd");
+            }
             // rozpoznawanie ifem
-            // +    -   *   /
-            Console.WriteLine("Kalkulator");
         }
     }
-    
