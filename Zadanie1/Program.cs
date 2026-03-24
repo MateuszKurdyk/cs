7f98dc662daaf307054df42713941bd5010481aa
Mateusz Kurdyk <matic@MacBook-Air-Mateusz.local>
2026-03-24 13:25:54 +0100

    plik z zadaniami c#

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+﻿namespace Zadanie1;
+
+    class ProstyKalkulator
+    {
+        static void Main()
+        {
+        
+            // inputy dla liczb i znakow
+            // rozpoznawanie ifem
+            // +    -   *   /
+            Console.WriteLine("Kalkulator");
+        }
+    }
